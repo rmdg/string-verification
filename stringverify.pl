7a0a19e873d811f13637f2ea62274a969a486fcb
root <root@DESKTOP-M3IBFA9.localdomain>
2018-02-04 11:17:54 -0800

    modify step 3 print valid codes

--- stringverify.pl
+++ stringverify.pl
@@ -7,6 +7,7 @@ my $input = "123456789123\n12345678912345\n123456x89123\n123456789\n";
 
 # Step 1: split the input string into individual array elements
 my @AoCodes = split("\n", $input);
+my @AoValidCodes;
 my $cnt_input_codes = ++$#AoCodes;
 print "Count of input codes:\t$cnt_input_codes\n";
 
@@ -19,14 +20,20 @@ foreach my $bc_validate ( @AoCodes ) {
 	my $ls=length($bc_validate);
 	if ( ($ls != 12) and ($ls != 14) ) { 
 		print "skipping:\t\"$bc_validate\" has invalid length\n";
+		next;
 	}
 
 	# check if element is non-numeric and if so then skip
 	if ( $bc_validate =~ m/[^0-9.]/ ) { 
 		print "skipping:\t\"$bc_validate\" contains non numeric characters\n";
+		next;
 	}
-	# Step 2b: push valid elements to return array
-
+	# Step 2b: push valid elements
+	push(@AoValidCodes,$bc_validate);
 }
 
-# Step 3: print out return array of valid codes
+# Step 3: print out array of valid codes
+print "\n\nThe following input codes are valid:\n";
+print "$_\n" foreach @AoValidCodes;
+
+

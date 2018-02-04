23754015da4282589690e979a5901ca3d67d939a
root <root@DESKTOP-M3IBFA9.localdomain>
2018-02-04 10:38:39 -0800

    add step 1

--- stringverify.pl
+++ stringverify.pl
@@ -5,10 +5,16 @@ use strict;
 # assign input string to test multiple valid and invalid strings
 my $input = "123456789123\n12345678912345\n123456x89123\n123456789\n";
 
+# Step 1: split the input string into individual array elements
 my @AoCodes = split("\n", $input);
-print ++$#AoCodes;
+my $cnt_input_codes = ++$#AoCodes;
+print "Count of input codes:\t$cnt_input_codes\n";
 
-foreach $bc_validate ( @AoCodes ) {
-	# Step 1: disregard elements not equal to either 12 or 14 numeric characters
-	# Step 2: push valid elements to return array
+
+# Step 2: validate each input element and push to array of valid codes
+foreach my $bc_validate ( @AoCodes ) {
+	# Step 2a: disregard elements not equal to either 12 or 14 numeric characters
+	# Step 2b: push valid elements to return array
 }
+
+# Step 3: print out return array of valid codes
